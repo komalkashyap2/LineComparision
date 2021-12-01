9c7c08d354dcf14a79170cba6387e427145ef479
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-01 21:30:08 +0530

    [KOMAL KASHYAP] Added two coordinates are equal

---
+++ Coordinates1/Coordinates1/coordinates2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates1
+{
+    public class coordinates2
+    {
+        static void Main(string[] args)
+        {
+            string x1 = "x1";
+            string x2 = "x1";
+            Console.WriteLine(x1 == x2);
+            Console.WriteLine(x1.Equals(x2));
+            Console.WriteLine(x1.GetHashCode() + "," + x2.GetHashCode());
+
+            string y1 = "x1";
+            Console.WriteLine(x1 == y1);
+            Console.WriteLine(x1.Equals(y1));
+            Console.WriteLine(x1.GetHashCode() + "," + y1.GetHashCode());
+
+            string y2 = "y1";
+            Console.WriteLine(x1 == y2);
+            Console.WriteLine(x1.Equals(y2));
+            Console.WriteLine(x1.GetHashCode() + "," + y2.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Coordinates2/Coordinates2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                string x1 = "x1";
+                string x2 = "x1";
+                Console.WriteLine(x1 == x2);
+                Console.WriteLine(x1.Equals(x2));
+                Console.WriteLine(x1.GetHashCode() + "," + x2.GetHashCode());
+
+                string y1 = "x1";
+                Console.WriteLine(x1 == y1);
+                Console.WriteLine(x1.Equals(y1));
+                Console.WriteLine(x1.GetHashCode() + "," + y1.GetHashCode());
+
+                string y2 = "y1";
+                Console.WriteLine(x1 == y2);
+                Console.WriteLine(x1.Equals(y2));
+                Console.WriteLine(x1.GetHashCode() + "," + y2.GetHashCode());
+                Console.ReadLine();
+            }
+        }
+    }
+}
