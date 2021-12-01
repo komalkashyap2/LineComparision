cfd50efd9a70359f4c34f3d20aa9531db1ec5097
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-01 21:37:00 +0530

    [KOMAL KASHYAP] Added two coordinates and compared

---
+++ Coordinates3/Coordinates3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates3
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            short len1 = 101;
+            short len2 = 103;
+
+            Console.WriteLine("length 1" + len1);
+            Console.WriteLine("length 2" + len2);
+            Console.WriteLine("value of two length+" + len1.CompareTo(len2));
+        }
+    }
+}
